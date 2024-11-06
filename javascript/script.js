d4909a6d2c030197fbe1b7aff84133db97b80307
KrushnaBarhate03 <krushnabarhate822@gmail.com>
2024-11-06 08:35:59 +0530

    assignment33 completed

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+function OpenButton(){
+  const BlurElement = document.getElementById('background-blur');
+  BlurElement.style.display = 'flex';
+}
+function CloseButton() {
+  const BlurElement = document.getElementById('background-blur');
+  BlurElement.style.display = "none";
+}
+
